5e48e6edf4b611d3807bc8e2fc194d75c1635b91
babelpainterwell <zhang.zhongw@northeastern.edu>
2024-10-31 00:21:53 -0400

    state transtion

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Raft
+
+## Q & A
+
+1. How is message considered as comitted?
+
+- An entry is considered committed if it is safe for that entry to be applied to state machines.
+- Logs that have been appended to a local server are not necessarily considered committed until they are confirmed by a majority of servers in the cluster.
+
+2. Any timeout could lead to leader re-election?
+
+- If a follower receives no communication over a period of time called the election timeout, then it assumes there is no vi- able leader and begins an election to choose a new leader.
+
+3. When a Server Receives a RequestVote RPC, Must It Vote Yes?
+
+- Not necessarily. a.The requesting candidate's term must be at least as high as the receiver's current term. b. The candidate’s log must be at least as up-to-date as the receiver’s. If these conditions are met and the server hasn’t already voted in the current term, it will vote for the candidate.
+- If it has already voted for iteself, it cannot vote for another candidate in that term.
+
+4. A mechnism for a candidate to become a follower after split votes, otherwise indefinite split votes?
+
+- Yes, if a candidate sees a higher term in a RequestVote or AppendEntries message from another server, it immediately steps down (from candidate or leader) to the follower state, recognizing the other server’s term as more up-to-date.
--- raft.go
+++ raft.go
@@ -1,1 +1,99 @@
 package raft
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type State int 
+
+const (
+	Follower State = iota 
+	Candidate 
+	Leader 
+)
+
+type Raft struct {
+	mu sync.Mutex
+	id int
+	term int 
+	state State 
+	votes int 
+	electionTimer *time.Timer
+	heartbeatInterval time.Duration
+}
+
+func GenerateRandom(lowerBound int64, upperBound int64) int64 {
+	// to ensure different random values for each server 
+	// rand.Seed(time.Now().UnixNano()) --> deprecared 
+	// create a random number generator 
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return rng.Int63n(upperBound - lowerBound) + lowerBound
+
+}
+
+// Set up a new Raft istance with a random election timeout between 150 - 300 ms
+func NewRaft(id int) *Raft {
+
+	randomNum := GenerateRandom(150, 300)
+	minTimeout := 150 * time.Millisecond
+	electionTimeout := minTimeout + time.Duration(randomNum)
+
+	return &Raft{
+		id: id,
+		term: 0, 
+		state: Follower,
+		votes: 0,
+		electionTimer: time.NewTimer(electionTimeout),
+		heartbeatInterval: 50 * time.Millisecond,
+	}
+}
+
+// reset a randomized timer in case of term resets, failures, or state changes 
+func (node *Raft) ResetElectionTimer() {
+	randomNum := GenerateRandom(150, 300)
+	minTimeout := 150 * time.Millisecond
+	electionTimeout := minTimeout + time.Duration(randomNum)
+	node.electionTimer.Reset(electionTimeout)
+	fmt.Printf("Server %d: Reset election timer to %v\n", node.id, electionTimeout)
+}
+
+// Methods for state transtions (ONLY), excluding operations such as reset timer 
+// without any RPC communication. eg. new candidate sends RequestVote, new leader sends empty AppendEntry RPC (heartbeat)
+func (node *Raft) FollowerToCandidate(){
+	node.state = Candidate
+
+	// increase its term 
+	node.term += 1
+
+	// candidate votes for itself first 
+	node.votes = 1
+
+	// restart the election timeout and it waits the timeout to eplase before starting a new election
+	node.ResetElectionTimer()
+
+	fmt.Printf("Server %d becomes a candidate\n", node.id)
+}
+
+func (node *Raft) CandidateToLeader(){
+	node.state = Leader 
+	fmt.Printf("Server %d becomes a leader\n", node.id)
+}
+
+// once it receives majority votes, it becomes the leader and starts sending heartbeats
+func (node *Raft) LeaderToFollower(){
+	node.state = Follower
+	// reset the votes
+	node.votes = 0 
+
+	fmt.Printf("Server %d becomes a follower\n", node.id)
+}
+
+func (node *Raft) CandidateToFollower() {
+	node.state = Follower
+
+	fmt.Printf("Server %d becomes a follower\n", node.id)
+}
+
